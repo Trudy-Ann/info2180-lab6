1c8ad5412fc370e3dfeccb74f2feb06c586ba83c
summerstarchica <trudyanncampbell.402@gmail.com>
2016-11-12 03:49:06 +0000

    attempting to get all definitions

--- request.php
+++ request.php
@@ -1,4 +1,20 @@
 <?php
+if (isset($_GET['all']) && $_GET['all'] == 'true') {
+    
+    $defStr = '<?xml version="1.0" encoding="UTF-8"?> 
+<entries> 
+    <definition name ="definition" author="John"> a statement of the exact meaning of a word, especially in a dictionary. </definition>
+    <definition name ="bar" author="mary"> a place that sells alcholic beverages </definition>
+    <definition name ="ajax" author="Kimberly">technique which involves the use of javascript and xml</definition>
+    <definition name ="css" author="Topaz">A style sheet language used for describing the presentation of a document written in a markup language.</definition>
+    <definition name = "javascript" author="Kevin"> lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</definition>
+    <definition name = "php" author="chica">A server-side scripting language, and a powerful tool for making dynamic and interactive websites</definition>
+</entries>';
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($defStr);
+echo $xmlOutput->asXML();
+    
+}
 // accept a term (keyword)
 // respond with a value
 $query = $_GET['q'];
